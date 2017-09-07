d0e9030648031857609f27824f5d9530f6a79d01
Piotr Bazydlo <pbazydlo@opentable.com>
2017-09-07 13:47:17 +0100

    Add acceptance tests for coverage in query generator

--- test/acceptance/commentTests.js
+++ test/acceptance/commentTests.js
@@ -22,7 +22,7 @@ describe('Query generation', () => {
 
   it('Ignores fields with +NOFOLLOW in description', () => {
     return queryPromise
-      .then(({queries}) => {
+      .then(({queries, coverage}) => {
         (queries[0].match(/ignoredWithExamples/g) || []).length.should.equal(0);
         (queries[0].match(/ignoredNoParameters/g) || []).length.should.equal(0);
       });
@@ -33,6 +33,19 @@ describe('Query generation', () => {
       .then(({queries}) => {
         // 8 because we have two examples for rollXTimes and 4 examples of parent
         (queries[0].match(/rollXTimes\(times. [0-9]+\)/g) || []).length.should.equal(8);
+      });
+  });
+
+  it('Calculates valid coverage', () => {
+    return queryPromise
+      .then(({coverage}) => {
+        coverage.coverageRatio.should.be.at.least(0);
+        coverage.coverageRatio.should.be.at.most(1);
+        if(coverage.coverageRatio < 1.0) {
+          coverage.notCoveredFields.length.should.be.at.least(1);
+        } else {
+          coverage.notCoveredFields.length.should.equal(0);
+        }
       });
   });
 
