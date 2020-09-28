2df4a65dd677b799a5230ac926bb8d56d652d9f7
Chad Bumstead <cbumstead@gmail.com>
2020-09-28 11:57:03 -0700

    add comments

--- src/queryGenerator.ts
+++ src/queryGenerator.ts
@@ -11,6 +11,7 @@ module.exports = function QueryGenerator(url) {
   }
 
   this.run = function () {
+    // Make a GraphQL POST passing in an introspection query to get back the schema
     return query(url, introspectionQuery)
       .then((res) => {
         if (!res.ok) {
@@ -24,7 +25,10 @@ module.exports = function QueryGenerator(url) {
       .then(result => {
         const queryTypeName = result.data['__schema'].queryType.name;
         const typeDictionary = buildTypeDictionary(result.data['__schema']);
+        // Each query can contain one or more tests.
+        // Multiple queries are prefixed to prevent name collisions
         const queries = schemaToQueries(queryTypeName, typeDictionary);
+        // Coverage stats do not take into consideration multiple tests on same query endpoint
         const coverage = calculateCoverage(queryTypeName, typeDictionary);
         return { queries, coverage };
       });
--- src/schemaToQueries.ts
+++ src/schemaToQueries.ts
@@ -6,7 +6,32 @@ export default function schemaToQueries (rootName, schema) {
   const queries : Array<string> = [];
   const sharedSkipList = [];
 
+  // Go through all the queries 
+  /***
+   * type Query {
+   * 
+   *  # Examples:
+   *  # track(id: "9108955fe473f1640ac38b9c")
+   *  track(id: ID!): Track
+   * 
+   *  # Examples:
+   *  # playlist(id: "9108955fe473f1640ac38b9c")
+   *  playlist(id: ID!): Playlist
+   * 
+   * }
+   */
   _.forIn(schema[rootName].fields, (field) => {
+    // Build the query to include all the available fields in the response
+    /***
+     * {
+     *   track(id: "638eb16ccb4fa02cc8c59bb6") {
+     *     id
+     *     title
+     *     ...
+     *  }  
+     * }
+     */
+
     const queryTree = schemaToQueryTree.buildQueryTreeFromField(field, schema, sharedSkipList);
 
     if (queryTree !== null) {
