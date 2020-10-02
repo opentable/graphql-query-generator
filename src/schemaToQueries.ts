0064dc471d3d07e9fd04469ebba17ca14c81f83d
Chad Bumstead <cbumstead@gmail.com>
2020-10-02 12:07:24 -0700

    output a query for each eample query

--- src/schemaToQueries.ts
+++ src/schemaToQueries.ts
@@ -1,23 +1,23 @@
 import queryTreeToGraphQLString from './aqtToQuery';
 import schemaToQueryTree from './schemaToQueryTree';
-import * as  _ from 'lodash';
+import * as _ from 'lodash';
 
-export default function schemaToQueries (rootName, schema) {
-  const queries : Array<string> = [];
+export default function schemaToQueries(rootName, schema) {
+  let queries: Array<string> = [];
   const sharedSkipList = [];
 
-  // Go through all the queries 
+  // Go through all the queries
   /***
    * type Query {
-   * 
+   *
    *  # Examples:
    *  # track(id: "9108955fe473f1640ac38b9c")
    *  track(id: ID!): Track
-   * 
+   *
    *  # Examples:
    *  # playlist(id: "9108955fe473f1640ac38b9c")
    *  playlist(id: ID!): Playlist
-   * 
+   *
    * }
    */
   _.forIn(schema[rootName].fields, (field) => {
@@ -28,16 +28,25 @@ export default function schemaToQueries (rootName, schema) {
      *     id
      *     title
      *     ...
-     *  }  
+     *  }
      * }
      */
 
     const queryTree = schemaToQueryTree.buildQueryTreeFromField(field, schema, sharedSkipList);
 
     if (queryTree !== null) {
-      queries.push(`{ ${queryTreeToGraphQLString(queryTree)} }`);
+      // Handle array OR objects
+      if (Array.isArray(queryTree)) {
+        queries = queries.concat(queryTree.map((query) => `{ ${queryTreeToGraphQLString(query)} }`));
+      } else {
+        for (const key in queryTree) {
+          const value = queryTree[key];
+          const query = `{ ${queryTreeToGraphQLString({ [key]: value })} }`;
+          queries.push(query);
+        }
+      }
     }
   });
 
   return queries;
-};
+}
