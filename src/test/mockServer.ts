0276f0c7a1140fe416ba876e0c75625f0c2472d3
Chad Bumstead <cbumstead@gmail.com>
2021-04-20 11:06:01 -0700

    fix ensure minimum test

--- src/test/mockServer.ts
+++ src/test/mockServer.ts
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
-import { mockServer } from 'graphql-tools';
+import { MockList, mockServer } from 'graphql-tools';
 
 export function mockPlaylistServer() {
   // Load graphql schema file from src folder as TS doesn't copy it to lib
@@ -12,9 +12,46 @@ export function mockPlaylistServer() {
     //   tag: 'Hello',
     // }),
     // ID: () => 'A',
-    // Playlist: {
-    //   tracks: () => [{}, {}, {}],
-    // },
+    RootQuery: () => ({
+      searchPlaylists: () => new MockList([2, 4]),
+      // searchPlaylists: () => [
+      //   {
+      //     name: 'playlist',
+      //     tracks: [
+      //       [
+      //         {
+      //           title: 'test',
+      //         },
+      //         {
+      //           title: 'test',
+      //         },
+      //         {
+      //           title: 'test',
+      //         },
+      //       ],
+      //     ],
+      //   },
+      // ],
+    }),
+    // Playlist: () => ({
+    //   tracks: () => new MockList([2, 3]),
+    // }),
+    // Playlist: () => ({
+    //   tracks: () => [
+    //     {
+    //       title: 'test',
+    //     },
+    //     {
+    //       title: 'test',
+    //     },
+    //     {
+    //       title: 'test',
+    //     },
+    //   ],
+    // }),
+    // Track: () => ({
+    //   title: () => 'test',
+    // }),
   };
 
   return mockServer(typeDefs, mocks, true);
